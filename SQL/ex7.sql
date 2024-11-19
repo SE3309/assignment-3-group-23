96bc918aae0f7f132b2577963b5a6087981f5a5c
tmaddox23 <tmaddox@uwo.ca>
2024-11-19 15:40:27 -0500

    adding a view to the ex7 file

--- SQL/ex7.sql
+++ SQL/ex7.sql
@@ -1,1 +1,5 @@
-
+CREATE VIEW usersTotalComments (UserID, UserName, count)
+AS SELECT u.userId, u.userName, COUNT(*) 
+FROM User u 
+JOIN Comment c ON u.userId = c.userId 
+GROUP BY u.userId, u.userName;
