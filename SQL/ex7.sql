65fbcb9c8e89a0918d3ae98f27088f2ae2214687
tmaddox23 <tmaddox@uwo.ca>
2024-11-19 16:07:45 -0500

    updating ex7 file with a second view

--- SQL/ex7.sql
+++ SQL/ex7.sql
@@ -3,3 +3,27 @@ AS SELECT u.userId, u.userName, COUNT(*)
 FROM User u 
 JOIN Comment c ON u.userId = c.userId 
 GROUP BY u.userId, u.userName;
+
+-- this will get the number of comments a user has created
+
+CREATE VIEW postSummary (PostID, PostDescription, CreatorName, CommentCount, IsPopular) AS
+SELECT 
+    p.postId,
+    p.description,
+    u.userName,
+    COUNT(*),
+    CASE 
+        WHEN COUNT(*) > 5 THEN 'Yes'
+        ELSE 'No'
+    END AS IsPopular
+FROM Post p
+JOIN User u ON p.userId = u.userId
+LEFT JOIN Comment c ON p.postId = c.postId
+GROUP BY p.postId, p.description, u.userName;
+
+/*
+	this will provide a summary of posts and their engagement
+    Join user to get the users name 
+    Join Comment to the count the number comments on each post
+    join left to to include points with no comments
+*/
