d427fb7a67b3beb7c7e71ea325cc488ec5cc939c
tmaddox23 <tmaddox@uwo.ca>
2024-11-19 16:31:51 -0500

    adding another complex view table using multiple tables and functions

--- SQL/ex7.sql
+++ SQL/ex7.sql
@@ -6,8 +6,13 @@ GROUP BY u.userId, u.userName;
 
 -- this will get the number of comments a user has created
 
-CREATE VIEW postSummary (PostID, PostDescription, CreatorName, CommentCount, IsPopular) AS
-SELECT 
+CREATE VIEW postSummary (
+	PostID, 
+	PostDescription,
+    CreatorName, 
+    CommentCount, 
+    IsPopular) 
+AS SELECT 
     p.postId,
     p.description,
     u.userName,
@@ -27,3 +32,35 @@ GROUP BY p.postId, p.description, u.userName;
     Join Comment to the count the number comments on each post
     join left to to include points with no comments
 */
+
+CREATE VIEW locationRiskWeatherSummary (
+    LocationName,
+    Country,
+    AverageWaveSize,
+    MaxWindSpeed,
+    RiskCount,
+    HasSharks
+) AS
+SELECT 
+    l.locationName,
+    l.countryName,
+    AVG(w.waveSize),
+    MAX(w.windSpeed),
+    COUNT(r.riskType),
+    CASE 
+        WHEN SUM(CASE WHEN r.riskType = 'Sharks' THEN 1 ELSE 0 END) > 0 THEN 'Yes'
+        ELSE 'No'
+    END AS HasSharks
+FROM SurfLocation l
+LEFT JOIN Weather w ON l.locationName = w.locationName
+-- all locations even if no weather
+LEFT JOIN Risks r ON l.locationName = r.locationName
+-- ensure all locations even if no risks
+GROUP BY l.locationName, l.countryName;
+
+/*
+	This view summarizes surf location weather and risk information 
+    into 1 table it will give you the name, country, aveage size, the max wind
+	, the total risks, and if it has sharks by checking if more then 1 occurence
+*/
+    
